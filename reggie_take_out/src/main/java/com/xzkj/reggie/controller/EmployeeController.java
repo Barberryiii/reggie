c8253ee17c44459da6b91ba236b1c14b114bc80c
liangxu <574910833@qq.com>
2022-05-24 09:19:57 +0800

    feat: 员工模块增加登录逻辑

--- reggie_take_out/src/main/java/com/xzkj/reggie/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/xzkj/reggie/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package com.xzkj.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.xzkj.reggie.common.R;
+import com.xzkj.reggie.entity.Employee;
 import com.xzkj.reggie.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
@@ -12,4 +20,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
+
+    @PostMapping("/login")
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee){
+        String username = employee.getUsername();
+        String password = employee.getPassword();
+
+        // 1.将页面提交的密码password进行md5加密处理
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        // 2.根据页面提交的username查询数据库
+        LambdaQueryWrapper<Employee> qw = new LambdaQueryWrapper<>();
+        qw.eq(Employee::getUsername, username);
+        Employee one = employeeService.getOne(qw);
+
+        // 3.如果没有查询到返回登录失败结果
+        if(one == null)
+            return R.error("用户名或密码错误");
+
+        // 4.密码比对，如果不一致则返回登录失败结果
+        if(!one.getPassword().equals(password))
+            return R.error("用户名或密码错误");
+
+        // 5.查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+        if(one.getStatus() == 0)
+            return R.error("用户名或密码错误");
+
+        // 6.登录成功，将员工id存入Session并返回登陆成功结果
+        request.getSession().setAttribute("employee", one);
+
+        return R.success(one);
+    }
+
 }
--- reggie_take_out/src/main/java/com/xzkj/reggie/service/impl/EmployeeServiceImpl.java
+++ reggie_take_out/src/main/java/com/xzkj/reggie/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.xzkj.reggie.entity.Employee;
 import com.xzkj.reggie.mapper.EmployeeMapper;
 import com.xzkj.reggie.service.EmployeeService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
 }
