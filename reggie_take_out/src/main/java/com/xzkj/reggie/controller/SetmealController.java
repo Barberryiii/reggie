8d9c3716f476a9be7dd851961f33224c199d00bb
liangxu <574910833@qq.com>
2022-05-25 21:46:06 +0800

    feat: 新增套餐模块

---
+++ reggie_take_out/src/main/java/com/xzkj/reggie/controller/SetmealController.java
@@ -0,0 +1,17 @@
+package com.xzkj.reggie.controller;
+
+import com.xzkj.reggie.service.SetmealDishService;
+import com.xzkj.reggie.service.SetmealService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/setmeal")
+public class SetmealController {
+    @Autowired
+    private SetmealService setmealService;
+
+    @Autowired
+    private SetmealDishService setmealDishService;
+}
---
+++ reggie_take_out/src/main/java/com/xzkj/reggie/service/impl/SetmealDishServiceImpl.java
@@ -0,0 +1,11 @@
+package com.xzkj.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.xzkj.reggie.entity.SetmealDish;
+import com.xzkj.reggie.mapper.SetmealDishMapper;
+import com.xzkj.reggie.service.SetmealDishService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetmealDishServiceImpl extends ServiceImpl<SetmealDishMapper, SetmealDish> implements SetmealDishService {
+}
